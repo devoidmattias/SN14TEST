f7544094d3c7c84f6ffc9ed4086d80567e194405
Mattias Thander <mattias@devoid.se>
2015-01-07 13:11:01 +0100

    Lagt till project AdderaTal

---
+++ AdderaTal/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace AdderaTal
+{
+    class Program
+    {
+        // Main är den metod där exekveringen börjar
+        static void Main(string[] args)
+        {
+            // Deklaration och initiering av variabler
+            int forstaTalet = 27;
+            int andraTalet = 15;
+            int summa = 0;
+
+            // Summera och tilldela till variabeln summa
+            summa = forstaTalet + andraTalet;
+
+            // Två variabler adderas och summan skrivs ut
+            Console.WriteLine("Summan är {0}", summa);
+        }
+    }
+}
